0358191571b13832140be5b3eac5e320f2723496
MagicExist <johhanparra1@gmail.com>
2025-09-17 21:33:07 -0500

    feat:add custom UserManager

--- back-end/core/models.py
+++ back-end/core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils import timezone
+from decimal import Decimal
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+#Custom UserManager
+class UserManager(BaseUserManager):
+    def create_user(self, email, password,**extra_fields):
+        if not email:
+            raise ValueError("The email is required")
+        if not password:
+            raise ValueError("The password is required")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self,email,password,**extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
