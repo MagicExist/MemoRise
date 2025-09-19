67fd2e86cbab4e77f98387c21a0e8c19584457e0
MagicExist <johhanparra1@gmail.com>
2025-09-18 23:46:25 -0500

    feat:Add CustomObtainPairView into the api token url

--- back-end/MemoRiseApi/urls.py
+++ back-end/MemoRiseApi/urls.py
@@ -16,6 +16,8 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from core.views import CustomTokenObtainPairView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,8 +25,8 @@ urlpatterns = [
 
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+
+    path("api/token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
