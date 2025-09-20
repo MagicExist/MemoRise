266c5bca9d2e543d4fcc4584720b80c9baa29782
MagicExist <johhanparra1@gmail.com>
2025-09-19 19:18:58 -0500

    feat:Add JWT authentication into user endpoints

--- back-end/core/views.py
+++ back-end/core/views.py
@@ -3,12 +3,14 @@ from .models import User
 from .serializer import PublicUserSerializer,CustomTokenObtainPairSerializer
 
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # User View Set (Here we will define the api endpoints for our user custom model)
 class PublicUserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = PublicUserSerializer
     http_method_names = ['get','post']
+    permission_classes = [IsAuthenticated]
     
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
