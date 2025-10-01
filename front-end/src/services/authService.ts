6447bf089c6426bc7198a2b63ae6ae242c1060a6
MagicExist <johhanparra1@gmail.com>
2025-10-01 08:12:48 -0500

    feat: Add loginUser request with axios

--- front-end/src/services/authService.ts
+++ front-end/src/services/authService.ts
@@ -1,7 +1,12 @@
 import api from "../api/axios";
-import type { UserRegistration } from "../types/user";
+import type { UserRegistration,UserLogin } from "../types/user";
 
 export const registerUser = async (userData: UserRegistration) => {
   const response = await api.post("/users/", userData);
+  return response.data;
+};
+
+export const loginUser = async (credentials: UserLogin) => {
+  const response = await api.post("/token/", credentials);
   return response.data;
 };
