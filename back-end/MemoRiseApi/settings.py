6a8e386805ea6603b173d46f8e11b27f889e3c21
MagicExist <johhanparra1@gmail.com>
2026-01-14 12:07:38 -0500

    feat:add railway host

--- back-end/MemoRiseApi/settings.py
+++ back-end/MemoRiseApi/settings.py
@@ -18,7 +18,7 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-OPENAI_API_KEY = config("OPENAI_API_KEY")
+OPENAI_API_KEY = OPENAI_API_KEY = config("OPENAI_API_KEY", default="")
 
 #Configuring Media urls to store media files
 MEDIA_URL = "/media/"
@@ -33,7 +33,9 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "memorise.up.railway.app"
+]
 
 
 # Application definition
